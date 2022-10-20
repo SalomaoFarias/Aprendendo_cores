4bad85d431d0e0e29df3f3280d65efaa727e293e
SalomaoFarias <112877293+SalomaoFarias@users.noreply.github.com>
2022-10-20 14:02:36 -0300

    Update Dificil_1Activity.java

--- app/src/main/java/br/fmu/aprendendo_cores/Dificil_1Activity.java
+++ app/src/main/java/br/fmu/aprendendo_cores/Dificil_1Activity.java
@@ -10,23 +10,94 @@ import android.widget.ImageView;
 public class Dificil_1Activity extends AppCompatActivity {
     private int valCor = 0;
 
-    private ImageView voltarMenu2;
+    private ImageView voltarMenu;
+    private ImageView cor1;
+    private ImageView cor2;
+    private ImageView cor3;
+    private ImageView cor4;
+    private ImageView cor5;
+    private ImageView cor6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificil1);
 
-        /* Imagem da seta pra esquerda */
-        voltarMenu2 = findViewById(R.id.voltarMenu);
+        /* verificar qual modo(nuturno ou diurno) está ativado */
 
+        /* Imagem da seta pra esquerda */
+        voltarMenu = findViewById(R.id.voltarMenuDifi);
+        
         // Troca para a tela Menu
-        voltarMenu2.setOnClickListener(new View.OnClickListener(){
+        voltarMenu.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-               menu();
+                menu();
             }
         });
+
+        /* Opções de cores */
+        cor1 = findViewById(R.id.cor1);
+        cor2 = findViewById(R.id.cor2);
+        cor3 = findViewById(R.id.cor3);
+        cor4 = findViewById(R.id.cor4);
+        cor5 = findViewById(R.id.cor5);
+        cor6 = findViewById(R.id.cor6);
+
+        cor1.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 0;
+                dificil_II();
+            }
+        });
+
+        cor2.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 1;
+                dificil_II();
+            }
+        });
+
+        cor3.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 2;
+                dificil_II();
+            }
+        });
+
+        cor4.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 3;
+                dificil_II();
+            }
+        });
+
+        cor5.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 4;
+                dificil_II();
+            }
+        });
+
+        cor6.setOnClickListener(new View.OnClickListener(){
+            @Overrride
+            public void onClick(View v) {
+                //Copiar e mandar a cor escolhida para Dificil_II
+                valCor = 5;
+                dificil_II();
+            }
+        });
+
     }
 
     public  void  menu() {
